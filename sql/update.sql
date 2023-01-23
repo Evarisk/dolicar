d95fbbe243ec5b543ae6a6f7057b9a62bcb6aa64
Théo David <theo.evarisk@gmail.com>
2023-01-23 09:31:22 +0100

    #111 [RegistrationCertificate] fix: update sql fields

--- sql/update.sql
+++ sql/update.sql
@@ -1,3 +1,10 @@
 ALTER TABLE `llx_dolicar_registrationcertificatefr` ADD `fk_product` integer NOT NULL AFTER `z4_specific_details`;
 ALTER TABLE `llx_dolicar_registrationcertificatefr` DROP `d3_vehicle_model`;
 ALTER TABLE `llx_dolicar_registrationcertificatefr` ADD `d3_vehicle_model` varchar(128) NULL AFTER `d21_vehicle_cnit`;
+
+-- 0.0.2
+ALTER TABLE llx_dolicar_registrationcertificatefr CHANGE c4a_owner_vehicle `c4a_vehicle_owner` boolean;
+ALTER TABLE llx_dolicar_registrationcertificatefr CHANGE c41_ownerNumber `c41_second_owner_number` integer;
+ALTER TABLE llx_dolicar_registrationcertificatefr CHANGE f1_techincal_ptac `f1_technical_ptac` integer;
+ALTER TABLE llx_dolicar_registrationcertificatefr CHANGE j_vehicleCategory `j_vehicle_category` varchar(128);
+ALTER TABLE llx_dolicar_registrationcertificatefr CHANGE s1_seatingCapacity `s1_seating_capacity` integer;
