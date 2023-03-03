9e146fc0459340fe1ce0d6616653bbc2baebdfec
Théo David <theo.evarisk@gmail.com>
2023-03-03 10:56:26 +0100

    #128 [Menu] add: minimize menu

--- core/modules/modDoliCar.class.php
+++ core/modules/modDoliCar.class.php
@@ -254,7 +254,8 @@ class modDoliCar extends DolibarrModules
             // This is a Left menu entry
             'type'=>'left',
             'titre' => $langs->trans('ListRegistrationCertificateFr'),
-            'mainmenu'=>'dolicar',
+			'prefix'   => '<i class="fas fa-car pictofixedwidth"></i>',
+			'mainmenu'=>'dolicar',
             'leftmenu'=>'dolicar_registrationcertificatefr',
             'url'=>'/dolicar/view/registrationcertificatefr/registrationcertificatefr_list.php',
             // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
@@ -267,26 +268,6 @@ class modDoliCar extends DolibarrModules
             'target'=>'',
             // 0=Menu for internal users, 1=external users, 2=both
             'user'=>2,
-        );
-        $this->menu[$r++]=array(
-            // '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
-            'fk_menu' => 'fk_mainmenu=dolicar,fk_leftmenu=dolicar_registrationcertificatefr',
-            // This is a Left menu entry
-            'type' => 'left',
-            'titre' => $langs->trans('NewRegistrationCertificateFr'),
-            'mainmenu'=>'dolicar',
-            'leftmenu'=>'dolicar_registrationcertificatefr',
-            'url'=>'/dolicar/view/registrationcertificatefr/registrationcertificatefr_card.php?action=create&fk_product='.$conf->global->DOLICAR_DEFAULT_VEHICLE,
-            // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
-            'langs'=>'dolicar@dolicar',
-            'position'=>1000+$r,
-            // Define condition to show or hide menu entry. Use '$conf->dolicar->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
-            'enabled'=>'$conf->dolicar->enabled',
-            // Use 'perms'=>'$user->rights->dolicar->level1->level2' if you want your menu with a permission rules
-            'perms'=>'1',
-            'target'=>'',
-            // 0=Menu for internal users, 1=external users, 2=both
-            'user'=>2
         );
 		$this->menu[$r++]=array(
 			'fk_menu' => 'fk_mainmenu=dolicar',
@@ -364,10 +345,10 @@ class modDoliCar extends DolibarrModules
 			'user'=>2
 		);
 		$this->menu[$r++]=array(
-			'fk_menu' => 'fk_mainmenu=dolicar,fk_leftmenu=dolicar_products',
+			'fk_menu' => 'fk_mainmenu=dolicar',
 			'type' => 'left',
 			'titre' => $langs->trans('Lot'),
-			'prefix'   => '<i class="fas fa-building paddingright pictofixedwidth"></i>',
+			'prefix'   => '<i class="fas fa-barcode paddingright pictofixedwidth"></i>',
 			'mainmenu'=>'dolicar',
 			'leftmenu'=>'dolicar_lot',
 			'url'=>'/product/index.php?mainmenu=products',
