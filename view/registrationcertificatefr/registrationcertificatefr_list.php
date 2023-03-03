ab362a16e94646b62f805708ccf796bd395b1820
Théo David <theo.evarisk@gmail.com>
2023-03-03 14:18:46 +0100

    #129 [RegistrationCertificate] fix: back url after creation

--- view/registrationcertificatefr/registrationcertificatefr_list.php
+++ view/registrationcertificatefr/registrationcertificatefr_list.php
@@ -435,7 +435,7 @@ print '<input type="hidden" name="sortorder" value="'.$sortorder.'">';
 print '<input type="hidden" name="page" value="'.$page.'">';
 print '<input type="hidden" name="contextpage" value="'.$contextpage.'">';
 
-$newcardbutton = dolGetButtonTitle($langs->trans('New'), '', 'fa fa-plus-circle', dol_buildpath('/dolicar/view/registrationcertificatefr/registrationcertificatefr_card.php', 1).'?action=create&fk_product='.$conf->global->DOLICAR_DEFAULT_VEHICLE. '&backtopage='.urlencode($_SERVER['PHP_SELF']), '', $permissiontoadd);
+$newcardbutton = dolGetButtonTitle($langs->trans('New'), '', 'fa fa-plus-circle', dol_buildpath('/dolicar/view/registrationcertificatefr/registrationcertificatefr_card.php', 1).'?action=create&fk_product='.$conf->global->DOLICAR_DEFAULT_VEHICLE, '', $permissiontoadd);
 
 print_barre_liste($title, $page, $_SERVER["PHP_SELF"], $param, $sortfield, $sortorder, $massactionbutton, $num, $nbtotalofrecords, 'object_'.$object->picto, 0, $newcardbutton, '', $limit, 0, 0, 1);
 
