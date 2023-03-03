781231a44c705f95e6de54f112ce7df011eb92d6
Théo David <theo.evarisk@gmail.com>
2023-03-03 09:27:57 +0100

    #126 [Index] fix: rework with framework

--- dolicar.main.inc.php
+++ dolicar.main.inc.php
@@ -1,6 +1,11 @@
 <?php
 
 $moduleName = 'DoliCar';
-$moduleNameLowerCase = 'dolicar';
+$moduleNameLowerCase = strtolower($moduleName);
 
-if (file_exists(__DIR__ . "/../saturne/saturne.main.inc.php")) $res = @include __DIR__ . "/../saturne/saturne.main.inc.php";
+// Load Saturne environment
+if (file_exists(__DIR__ . '/../saturne/saturne.main.inc.php')) {
+	require_once __DIR__ . '/../saturne/saturne.main.inc.php';
+} else {
+	die('Include of saturne main fails');
+}
