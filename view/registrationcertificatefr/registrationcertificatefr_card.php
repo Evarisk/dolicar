283238a93a6eda252f1f56a0a48ddbeaccbc69c6
Théo David <theo.evarisk@gmail.com>
2023-04-05 10:56:41 +0200

    #163 [RegistrationCertificate] fix: newProduct translation

--- view/registrationcertificatefr/registrationcertificatefr_card.php
+++ view/registrationcertificatefr/registrationcertificatefr_card.php
@@ -254,7 +254,7 @@ if ($action == 'create') {
 	$productPost = GETPOST('fk_product') ?: 0;
 	print '<tr><td class="">' . $langs->trans('LinkedProduct') . '</td><td>';
 	$form->select_produits($productPost, 'fk_product', '', 0, 1, -1, 2, '', '', '', '', 'SelectProductsOrServices', 0, 'maxwidth500 widthcentpercentminusxx', 1);
-	print '<a class="butActionNew" href="' . DOL_URL_ROOT . '/product/card.php?action=create&backtopage=' . urlencode($_SERVER['PHP_SELF'] . '?action=create') . '" target="_blank"><span class="fa fa-plus-circle valignmiddle paddingleft" title="' . $langs->trans('AddProduct') . '"></span></a>';
+	print '<a class="butActionNew" href="' . DOL_URL_ROOT . '/product/card.php?action=create&backtopage=' . urlencode($_SERVER['PHP_SELF'] . '?action=create') . '" target="_blank"><span class="fa fa-plus-circle valignmiddle paddingleft" title="' . $langs->trans('NewProduct') . '"></span></a>';
 	print '</td></tr>';
 
 	//Fk_lot
