3ce108dd941276df0b64629e859c5875ac920a7f
Théo David <theo.david@evarisk.com>
2023-04-06 09:47:28 +0200

    #126 [Index] add: framework index

--- dolicarindex.php
+++ dolicarindex.php
@@ -1,8 +1,5 @@
 <?php
-/* Copyright (C) 2001-2005 Rodolphe Quiedeville <rodolphe@quiedeville.org>
- * Copyright (C) 2004-2015 Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copyright (C) 2005-2012 Regis Houssin        <regis.houssin@inodbox.com>
- * Copyright (C) 2015      Jean-François Ferry	<jfefe@aternatik.fr>
+/* Copyright (C) 2021-2023 EVARISK <technique@evarisk.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,45 +16,16 @@
  */
 
 /**
- *	\file       dolicar/dolicarindex.php
+ *	\file       dolicarindex.php
  *	\ingroup    dolicar
  *	\brief      Home page of dolicar top menu
  */
 
-// Load DoliCar environment
+// Load DoliMeet environment
 if (file_exists('dolicar.main.inc.php')) {
 	require_once __DIR__ . '/dolicar.main.inc.php';
 } else {
 	die('Include of dolicar main fails');
 }
 
-global $conf, $db, $langs, $moduleName, $moduleNameLowerCase, $user;
-
-// Libraries
-require_once __DIR__ . '/core/modules/mod' . $moduleName . '.class.php';
-
-// Load translation files required by the page
-saturne_load_langs();
-
-// Initialize technical objects
-$classname = 'mod' . $moduleName;
-$modModule = new $classname($db);
-
-// Security check
-$permissiontoread = $user->rights->$moduleNameLowerCase->read;
-saturne_check_access($permissiontoread, null, true);
-
-/*
- * View
- */
-
-$title   = $langs->trans('ModuleArea', $moduleName);
-$helpUrl = 'FR:Module_' . $moduleName;
-
-saturne_header(0, '', $title . ' ' . $modModule->version, $helpUrl);
-
-print load_fiche_titre($title . ' ' . $modModule->version, '', $moduleNameLowerCase . '_color.png@' . $moduleNameLowerCase);
-
-// End of page
-llxFooter();
-$db->close();
+require_once __DIR__ . '/../saturne/core/tpl/index/index_view.tpl.php';
