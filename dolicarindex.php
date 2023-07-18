6475f588c2c01ce2b33902bf7c069e9c27ee75dd
Théo David <theo.evarisk@gmail.com>
2023-07-18 11:59:16 +0200

    #213 [Core] fix: framework compatibility

--- dolicarindex.php
+++ dolicarindex.php
@@ -30,4 +30,6 @@ if (file_exists('dolicar.main.inc.php')) {
 	die('Include of dolicar main fails');
 }
 
+$showDashboard = false;
+
 require_once __DIR__ . '/../saturne/core/tpl/index/index_view.tpl.php';
