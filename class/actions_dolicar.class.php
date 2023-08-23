5eb8aea88562cb4d4411355af23bae01ba9b05af
Théo David <theo.evarisk@gmail.com>
2023-08-23 16:23:44 +0200

    #233 [Hook] fix: show vehicle informations only if object has vehicle

--- class/actions_dolicar.class.php
+++ class/actions_dolicar.class.php
@@ -562,21 +562,24 @@ class ActionsDoliCar
 		|| (in_array('propalcard', explode(':', $parameters['context'])) && empty($conf->global->DOLICAR_HIDE_ADDRESS_ON_PROPALCARD))
 		|| (in_array('invoicecard', explode(':', $parameters['context'])) && empty($conf->global->DOLICAR_HIDE_ADDRESS_ON_INVOICECARD))
 		) {
-			require_once __DIR__ . '/../class/registrationcertificatefr.class.php';
-			require_once DOL_DOCUMENT_ROOT . '/product/stock/class/productlot.class.php';
-			$registrationcertificatefr = new RegistrationCertificateFr($this->db);
-			$productlot = new ProductLot($this->db);
-			$registrationcertificatefr->fetch($object->array_options['options_registrationcertificatefr']);
-			$productlot->fetch($registrationcertificatefr->fk_lot);
+            if ($object->array_options['options_registrationcertificatefr'] > 0) {
+                require_once __DIR__ . '/../class/registrationcertificatefr.class.php';
+                require_once DOL_DOCUMENT_ROOT . '/product/stock/class/productlot.class.php';
+                $registrationcertificatefr = new RegistrationCertificateFr($this->db);
+                $productlot = new ProductLot($this->db);
+                $registrationcertificatefr->fetch($object->array_options['options_registrationcertificatefr']);
+                $productlot->fetch($registrationcertificatefr->fk_lot);
 
-			$mileageWithSeparator = price($object->array_options['options_mileage'], 0,"",1, 0);
+                $mileageWithSeparator = price($object->array_options['options_mileage'], 0,"",1, 0);
 
-			$object->fetch_optionals();
-			$object->note_public = $langs->transnoentities('RegistrationNumber') . ' : ' . $object->array_options['options_registration_number'] . '<br>';
-			$object->note_public .= $langs->transnoentities('VehicleModel') . ' : ' . $object->array_options['options_vehicle_model'] . '<br>';
-			$object->note_public .= $langs->transnoentities('VINNumber') . ' : ' . $productlot->batch . '<br>';
-			$object->note_public .= $langs->transnoentities('FirstRegistrationDate') . ' : ' . dol_print_date($registrationcertificatefr->b_first_registration_date, 'day') . '<br>';
-			$object->note_public .= $langs->transnoentities('Mileage') . ' : ' . $mileageWithSeparator . ' ' . $langs->trans('km') . '<br>';
+                $object->fetch_optionals();
+                $object->note_public = $langs->transnoentities('RegistrationNumber') . ' : ' . $object->array_options['options_registration_number'] . '<br>';
+                $object->note_public .= $langs->transnoentities('VehicleModel') . ' : ' . $object->array_options['options_vehicle_model'] . '<br>';
+                $object->note_public .= $langs->transnoentities('VINNumber') . ' : ' . $productlot->batch . '<br>';
+                $object->note_public .= $langs->transnoentities('FirstRegistrationDate') . ' : ' . dol_print_date($registrationcertificatefr->b_first_registration_date, 'day') . '<br>';
+                $object->note_public .= $langs->transnoentities('Mileage') . ' : ' . $mileageWithSeparator . ' ' . $langs->trans('km') . '<br>';
+            }
+
 		}
 
 		return $ret;
