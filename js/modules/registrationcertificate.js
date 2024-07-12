afb2f7af2fb376650e0b49acfc98939dcc832659
Nicolas Domenech <nicolas@eoxia.com>
2024-07-12 17:19:45 +0200

    #244 [JS] add: window.dolicar.registrationcertificate.createPublicVehicleLogBook

--- js/modules/registrationcertificate.js
+++ js/modules/registrationcertificate.js
@@ -34,7 +34,21 @@ window.dolicar.registrationcertificate.event = function() {
 		$(document).find('.field_fk_project .butActionNew').attr('target', '_blank')
 	})
 
-}
+  $('#public-vehicle-log-book-form').on('submit', function(event) {
+    event.preventDefault();
+    if (!$(this).find('.public-vehicle-log-book-validate').hasClass('button-disable')) {
+      window.dolicar.registrationcertificate.createPublicVehicleLogBook();
+    }
+  });
+
+  $(document).on('touchstart mousedown', '.canvas-signature', function () {
+    window.saturne.toolbox.removeAddButtonClass('public-vehicle-log-book-validate', 'button-grey button-disable', 'button-blue');
+  });
+
+  $(document).on('click', '.signature-erase', function () {
+    window.saturne.toolbox.removeAddButtonClass('public-vehicle-log-book-validate', 'button-blue', 'button-grey button-disable');
+  });
+};
 
 
 /**
@@ -92,7 +106,7 @@ window.dolicar.registrationcertificate.actualizeProductlot = function( event ) {
 
 	let querySeparator =  window.saturne.toolbox.getQuerySeparator(document.URL)
 
-	window.saturne.loader.display($('.lot-content')); 
+	window.saturne.loader.display($('.lot-content'));
 
 	$.ajax({
 		url: document.URL + querySeparator + 'action=' + action + '&fk_product=' + productId + '&token='+token,
@@ -104,4 +118,41 @@ window.dolicar.registrationcertificate.actualizeProductlot = function( event ) {
 			$('.wpeo-loader').removeClass('wpeo-loader');
 		},
 	});
+};
+
+/**
+ * Create public vehicle log book entry
+ *
+ * @memberof DoliCar_RegistrationCertificate
+ *
+ * @since   1.2.0
+ * @version 1.2.0
+ *
+ * @return {void}
+ */
+window.dolicar.registrationcertificate.createPublicVehicleLogBook = function() {
+  let token          = window.saturne.toolbox.getToken();
+  let querySeparator = window.saturne.toolbox.getQuerySeparator(document.URL);
+
+  const formData = new FormData($('#public-vehicle-log-book-form')[0]);
+  if (window.saturne.signature.canvas) {
+    const signature = window.saturne.signature.canvas.toDataURL();
+    formData.append('signature', JSON.stringify(signature));
+  }
+
+  $.ajax({
+    url: document.URL + querySeparator + 'action=add&token=' + token,
+    type: 'POST',
+    processData: false,
+    contentType: false,
+    data: formData,
+    success: function() {
+      $('.card__confirmation').css('display', 'flex');
+      $('.public-vehicle-log-book-confirmation-close').on('click', function() {
+        $('.card__confirmation').css('display', 'none');
+        window.location.reload();
+      });
+    },
+    error: function() {}
+  });
 };
