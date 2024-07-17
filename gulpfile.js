f2415da3eb9ce9db9da0778c1e3c58c1c3a39fb2
Nicolas Domenech <nicolas@eoxia.com>
2024-07-17 14:42:55 +0200

    #268  [Mod] clean: config files and readme

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
 'use strict';
 
-var gulp         = require('gulp');
-var sass         = require('gulp-sass')(require('sass'));
-var rename       = require('gulp-rename');
-var uglify       = require('gulp-uglify');
-var concat       = require('gulp-concat');
+const gulp   = require('gulp');
+const sass   = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 
-var paths = {
-	scss_core : [ 'css/scss/**/*.scss', 'css/' ],
-	js_backend: [ 'js/dolicar.js', 'js/modules/*.js' ]
+const paths = {
+  scssCore : ['css/scss/**/*.scss', 'css/'],
+  jsBackend: ['js/dolicar.js', 'js/modules/*.js']
 };
 
 /** Core */
-gulp.task( 'scss_core', function() {
-	return gulp.src( paths.scss_core[0] )
-		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-		.pipe(rename('./dolicar.min.css'))
-		.pipe(gulp.dest( paths.scss_core[1]));
+gulp.task('scssCore', function() {
+  return gulp.src(paths.scssCore[0])
+    .pipe(sass({outputStyle: 'compressed'}, '').on('error', sass.logError))
+    .pipe(rename('./dolicar.min.css'))
+    .pipe(gulp.dest(paths.scssCore[1]));
 });
 
-gulp.task('js_backend', function () {
-	return gulp.src(paths.js_backend)
-		.pipe(concat('dolicar.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('./js/')) // It will create folder client.min.js
+gulp.task('jsBackend', function () {
+  return gulp.src(paths.jsBackend)
+    .pipe(concat('dolicar.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./js/')); // It will create folder client.min.js
 });
 
 /** Watch */
 gulp.task('default', function() {
-	gulp.watch(paths.scss_core[0], gulp.series('scss_core'));
-	gulp.watch(paths.js_backend[1], gulp.series('js_backend'));
+  gulp.watch(paths.scssCore[0], gulp.series('scssCore'));
+  gulp.watch(paths.jsBackend[1], gulp.series('jsBackend'));
 });
