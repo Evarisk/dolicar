85e0610835e9264dd8a02f6a76b917e4f99a0f1b
Nicolas Domenech <nicolas@eoxia.com>
2024-07-17 14:44:41 +0200

    #268 [SQL] clean: sql files

--- sql/llx_dolicar_registrationcertificatefr_extrafields.sql
+++ sql/llx_dolicar_registrationcertificatefr_extrafields.sql
@@ -1,8 +1,8 @@
--- Copyright (C) ---Put here your own copyright and developer email---
+-- Copyright (C) 2022-2024 EVARISK <technique@evarisk.com>
 --
--- This program is free software; you can redistribute it and/or modify
+-- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation; either version 3 of the License, or
+-- the Free Software Foundation, either version 3 of the License, or
 -- (at your option) any later version.
 --
 -- This program is distributed in the hope that it will be useful,
@@ -11,13 +11,12 @@
 -- GNU General Public License for more details.
 --
 -- You should have received a copy of the GNU General Public License
--- along with this program. If not, see https://www.gnu.org/licenses/.
+-- along with this program.  If not, see https://www.gnu.org/licenses/.
 
-create table llx_dolicar_registrationcertificatefr_extrafields
-(
-  rowid                     integer AUTO_INCREMENT PRIMARY KEY,
-  tms                       timestamp,
-  fk_object                 integer NOT NULL,
-  import_key                varchar(14)                          		-- import key
+create table llx_dolicar_registrationcertificatefr_extrafields(
+  rowid      integer AUTO_INCREMENT PRIMARY KEY,
+  tms        timestamp,
+  fk_object  integer NOT NULL,
+  import_key varchar(14)
 ) ENGINE=innodb;
 
