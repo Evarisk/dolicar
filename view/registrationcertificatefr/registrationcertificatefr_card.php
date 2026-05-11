b679fb8bbbada2800bc524271fa4011bd90f1a52
evarisk-kilyan <kilyan.evarisk@gmail.com>
2026-05-11 10:48:16 +0200

    #410 [RegistrationCertificateFr] feat: add merged actioncom list from card and vehicle history

--- view/registrationcertificatefr/registrationcertificatefr_card.php
+++ view/registrationcertificatefr/registrationcertificatefr_card.php
@@ -31,6 +31,7 @@ if (file_exists('../dolicar.main.inc.php')) {
 }
 
 // Load Dolibarr libraries
+require_once DOL_DOCUMENT_ROOT . '/comm/action/class/actioncomm.class.php';
 require_once DOL_DOCUMENT_ROOT . '/core/class/html.formcompany.class.php';
 require_once DOL_DOCUMENT_ROOT . '/core/class/html.formfile.class.php';
 require_once DOL_DOCUMENT_ROOT . '/core/class/html.formprojet.class.php';
@@ -512,7 +513,7 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
     // Linked objects section
     require_once __DIR__ . '/../../core/tpl/registrationcertificatefr_linked_objects.tpl.php';
 
-    // Documents section
+    // Documents and actions section
     if ($action != 'presend') {
         $dirOutput = !empty($conf->dolicar->multidir_output[$conf->entity]) ? $conf->dolicar->multidir_output[$conf->entity] : $conf->dolicar->dir_output;
         $dirFiles  = 'livretentretien/' . dol_sanitizeFileName($object->ref);
@@ -522,6 +523,89 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
         print '<div class="fichecenter">';
         print '<div class="fichehalfleft">';
         print saturne_show_documents('dolicar:Livretentretien', $dirFiles, $fileDir, $urlSource, $permissiontoadd, $permissiontodelete, getDolGlobalString('DOLICAR_LIVRETENTRETIEN_DEFAULT_MODEL'), 1, 0, 0, 0, 0, '', '', $langs->defaultlang, '', $object);
+        print '</div>';
+
+        print '<div class="fichehalfright">';
+        $MAXEVENT   = 10;
+        $actionComm = new ActionComm($db);
+
+        $filter  = " AND ((a.fk_element = " . (int) $object->id . " AND a.elementtype = '" . $db->escape($object->element) . "')";
+        if (!empty($object->fk_lot) && $object->fk_lot > 0) {
+            $filter .= " OR (a.fk_element = " . (int) $object->fk_lot . " AND a.elementtype = 'productlot')";
+        }
+        $filter .= ")";
+
+        $listofactions = $actionComm->getActions(0, 0, '', $filter, 'a.datep,a.id', 'DESC', $MAXEVENT + 1);
+        $num           = is_array($listofactions) ? count($listofactions) : 0;
+
+        $usercanaddaction = isModEnabled('agenda') && ($user->hasRight('agenda', 'myactions', 'create') || $user->hasRight('agenda', 'allactions', 'create'));
+        $morehtmlright    = '';
+        if ($usercanaddaction) {
+            $addUrl        = DOL_URL_ROOT . '/comm/action/card.php?action=create&token=' . newToken()
+                . '&origin=' . urlencode($object->element) . '&originid=' . (int) $object->id
+                . ($object->fk_soc > 0 ? '&socid=' . (int) $object->fk_soc : '')
+                . '&backtopage=' . urlencode($_SERVER['PHP_SELF'] . '?id=' . $object->id);
+            $morehtmlright = dolGetButtonTitle($langs->trans('AddEvent'), '', 'fa fa-plus-circle', $addUrl);
+        }
+
+        print load_fiche_titre($langs->trans('LatestLinkedEvents', $MAXEVENT), $morehtmlright, '', 0, '', '', '');
+        print '<div class="div-table-responsive-no-min">';
+        print '<table class="centpercent noborder listactions">';
+        print '<tr class="liste_titre">';
+        print '<td>' . $langs->trans('Ref') . '</td>';
+        print '<td class="center">' . $langs->trans('Date') . '</td>';
+        print '<td>' . $langs->trans('By') . '</td>';
+        print '<td class="center"></td>';
+        print '<td>' . $langs->trans('Title') . '</td>';
+        print '<td class="right"></td>';
+        print '</tr>';
+
+        if (is_array($listofactions) && !empty($listofactions)) {
+            $cacheusers  = [];
+            $cursorevent = 0;
+            foreach ($listofactions as $evt) {
+                if ($cursorevent >= $MAXEVENT) {
+                    break;
+                }
+                print '<tr class="oddeven">';
+                print '<td class="nowraponall nopaddingrightimp">' . $evt->getNomUrl(1, -1) . '</td>';
+                print '<td class="center nowraponall celldateheight">';
+                print dolOutputDates($evt->datep, $evt->datef, $evt->fulldayevent, 0, '', 'tzuserrel', 1);
+                print '</td>';
+                print '<td class="nowraponall tdoverflowmax100">';
+                if (!empty($evt->userownerid)) {
+                    if (!isset($cacheusers[$evt->userownerid])) {
+                        $tmpuser = new User($db);
+                        $tmpuser->fetch($evt->userownerid);
+                        $cacheusers[$evt->userownerid] = $tmpuser;
+                    }
+                    if ($cacheusers[$evt->userownerid]->id > 0) {
+                        print $cacheusers[$evt->userownerid]->getNomUrl(-1, '', 0, 0, 16, 0, 'firstelselast', '');
+                    }
+                }
+                print '</td>';
+                print '<td class="tdoverflowmax100 center valignmiddle" title="' . dolPrintHTML($evt->getTypeLabel(2)) . '">';
+                print $evt->getTypePicto('valignmiddle');
+                if (preg_match('/PRIVATE/', $evt->code)) {
+                    print ' ' . img_picto($langs->trans('Private'), 'lock', 'class="valignmiddle"');
+                }
+                print '</td>';
+                print '<td class="tdoverflowmax250">' . $evt->getNomUrl(0) . '</td>';
+                print '<td class="right">' . $evt->getLibStatut(3) . '</td>';
+                print '</tr>';
+                $cursorevent++;
+            }
+        } else {
+            print '<tr class="oddeven"><td colspan="6"><span class="opacitymedium">' . $langs->trans('None') . '</span></td></tr>';
+        }
+
+        if ($num > $MAXEVENT) {
+            print '<tr class="oddeven"><td colspan="6"><span class="opacitymedium">' . $langs->trans('More') . '...</span></td></tr>';
+        }
+
+        print '</table>';
+        print '</div>';
+
         print '</div>';
         print '</div>';
     }
